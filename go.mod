3f10de1fe4f61cbbafa18398402c44fb01285487
tawatchai7034 <tawatchaisunanta@gmail.com>
2025-01-05 14:44:05 +0700

    ApiAuthentication

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.3
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/liudng/dogo v0.0.0-20161102105440-7887b849de84 // indirect
 	github.com/zhgo/config v0.0.0-20150924145423-9caf41330bd9 // indirect
 	github.com/zhgo/console v0.0.0-20150817103316-22d59bcbd28e // indirect
--- http_sever/api.go
+++ http_sever/api.go
@@ -7,11 +7,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 )
 
 func UnmarshalEmployee(data []byte) (Employee, error) {
@@ -39,6 +42,76 @@ type ApiResponse struct {
 	Code    int64  `json:"Code"`
 	Message string `json:"Message"`
 	Result  int64  `json:"Result"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	empNoparam := http.HandlerFunc(empHandler)
+	empGetparam := http.HandlerFunc(empSingle)
+	//Authentication
+	http.HandleFunc("/login", ApiAuthentication)
+	// no param
+	http.Handle("/employee", middleware(empNoparam))
+	// get param
+	http.Handle("/employee/", middleware(empGetparam))
+	http.ListenAndServe(":5000", nil)
+}
+
+func ApiAuthentication(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		s := r.Header.Get("Authorization")
+		token := strings.TrimPrefix(s, "Bearer ")
+		if token != "" {
+			if err := validateToken(token); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+			} else {
+				res := ApiResponse{Code: 200, Message: "success", Result: 0}
+				resJson, err := json.Marshal(res)
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+				}
+				w.Write(resJson)
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+	case http.MethodPost:
+		// bodybtye, err := ioutil.ReadAll(r.Body)
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, &jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(5 * time.Minute).Unix(),
+		})
+		key := os.Getenv("JWT_SECRET_KEY")
+
+		ss, err := token.SignedString([]byte(key))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		resJson, err := json.Marshal(ss)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.Write(resJson)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func validateToken(token string) error {
+	key := os.Getenv("JWT_SECRET_KEY")
+	_, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+
+		return []byte(key), nil
+	})
+
+	return err
 }
 
 func getEmployee(w http.ResponseWriter, db *sql.DB) {
@@ -240,18 +313,18 @@ func empSingle(w http.ResponseWriter, r *http.Request) {
 
 func middleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("brfore handler middle ater")
-		handler.ServeHTTP(w, r)
-		fmt.Println("middleware finised")
-	})
-}
+		s := r.Header.Get("Authorization")
+		token := strings.TrimPrefix(s, "Bearer ")
+		if token != "" {
+			if err := validateToken(token); err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+			} else {
+				handler.ServeHTTP(w, r)
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+		}
 
-func main() {
-	empNoparam := http.HandlerFunc(empHandler)
-	empGetparam := http.HandlerFunc(empSingle)
-	// no param
-	http.Handle("/employee", middleware(empNoparam))
-	// get param
-	http.Handle("/employee/", middleware(empGetparam))
-	http.ListenAndServe(":5000", nil)
+		// fmt.Println("middleware finised")
+	})
 }
